2fed536efde93627166cf69d2bb8bc35767a4b5b
Ramin Yaghoubzadeh Torky <ramin.y.torky@nerian.com>
2024-01-04 11:59:53 +0100

    Added LibraryInfo class with static members to return versions (C++, Py)

---
+++ libvisiontransfer/visiontransfer/libraryinfo.cpp
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ * Copyright (c) 2024 Allied Vision Technologies GmbH
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *******************************************************************************/
+
+#include <visiontransfer/libraryinfo.h>
+
+#include <sstream>
+
+namespace visiontransfer {
+
+/* static */
+const char* LibraryInfo::getLibraryVersionString() {
+    static std::string ver("");
+    if (ver=="") {
+        std::stringstream ss;
+        ss << VISIONTRANSFER_MAJOR_VERSION << "." << VISIONTRANSFER_MINOR_VERSION << "." << VISIONTRANSFER_PATCH_VERSION;
+        ver = ss.str();
+    }
+    return ver.c_str();
+}
+
+
+} // namespace
+
---
+++ libvisiontransfer/visiontransfer/libraryinfo.h
@@ -0,0 +1,56 @@
+/*******************************************************************************
+ * Copyright (c) 2024 Allied Vision Technologies GmbH
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *******************************************************************************/
+
+#ifndef VISIONTRANSFER_LIBRARYINFO_H
+#define VISIONTRANSFER_LIBRARYINFO_H
+
+#include <visiontransfer/common.h>
+
+namespace visiontransfer {
+
+/**
+ * \brief Information about the current visiontransfer (API) library.
+ */
+class VT_EXPORT LibraryInfo {
+public:
+    /**
+     * \brief Return the version string for the visiontransfer library.
+     *
+     * \return The dotted version string as a static const char*
+     *  (which must not be freed).
+     */
+    static const char* getLibraryVersionString();
+
+    /**
+     * \brief Returns the major version of visiontransfer library
+     */
+    static int getLibraryVersionMajor() { return VISIONTRANSFER_MAJOR_VERSION; }
+
+    /**
+     * \brief Returns the minor version of visiontransfer library
+     */
+    static int getLibraryVersionMinor() { return VISIONTRANSFER_MINOR_VERSION; }
+
+    /**
+     * \brief Returns the patch version of visiontransfer library
+     */
+    static int getLibraryVersionPatch() { return VISIONTRANSFER_PATCH_VERSION; }
+};
+
+} // namespace
+
+
+#endif
+
+
